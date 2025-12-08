e6ecd54716e08a341b33afec9f6a30a0b057665d
Paul Amonson <pamonson@anaconda.com>
2025-12-08 09:20:19 -0800

    Try #7
    Signed-off-by: Paul Amonson <pamonson@anaconda.com>

--- scripts/make-docs.sh
+++ scripts/make-docs.sh
@@ -6,49 +6,6 @@ python=`which python`
 [[ -z "${python}" ]] && python=`which python3`
 
 # Bash function definitions.
-function mac_install_latex() {
-    # Install LaTeX on MacOS
-    local found=`brew list --cask | grep mactex`
-    [[ -z "${found}" ]] && brew install --cask mactex && return $?
-    echo "Latex for MacOS is already installed!"
-    return 0
-}
-
-function linux_install_latex() {
-    # Install LaTeX on Linux
-    local found=`apt list | grep latexmk 2>/dev/null`
-    [[ -z "${found}" ]] && sudo apt-get install -y texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended latexmk && return $?
-    echo "Latex for Sphinx is already installed!"
-    return 0
-}
-
-function windows_install_latex() {
-    # Install LaTeX on Windows
-    echo "ERROR: Windows unsupported for PDF builds!" >&2
-    exit -1  # Return Error Code
-}
-
-function install_latex() {
-    # Install LaTeX based on the operating system
-    case $UNAME in
-        Darwin)
-            mac_install_latex
-            ;;
-        Linux)
-            linux_install_latex
-            ;;
-        MINGW*|MSYS*)
-            windows_install_latex
-            [[ $? -ne 0 ]] && exit -1  # Return Error Code (Windows currently not supported)
-            ;;
-        *)
-            echo "ERROR: Unsupported OS!" >&2
-            exit -1
-            ;;
-    esac
-    return $?
-}
-
 function pip_exists() {
     # Check if a pip package exists
     local found=`pip3 list | awk '{print $1}' | grep -e "^$1$"`
@@ -98,7 +55,6 @@ replace_version "${__SDK_VERSION__}"
 # Install dependencies if needed
 CONDA=`which conda`
 [[ -z "${CONDA}" ]] && echo "ERROR: conda not found! Please install the latest version of miniconda from https://anaconda.com/." >&2 && restore_version && exit 1
-install_latex
 pip_install
 
 # Clean up previous builds and prepare for new build
@@ -127,20 +83,10 @@ cd docs
 rm -rf build
 sphinx-build -M html "source" "build"
 [[ $? -ne 0 ]] && echo "ERROR: sphinx build failed to produce HTML output!" >&2 && restore_versiobnt && exit 1
-sphinx-build -M latexpdf "source" "build"
-[[ $? -ne 0 ]] && echo "ERROR: sphinx build failed to produce PDF output!" >&2 && restore_version && exit 1
-cd ..
 
 # Copy documentation to the 'dist' folder
 tar -caf dist/anaconda-opentelemetry-${__SDK_VERSION__}-html-doc.tar.gz docs/build/html
 cp docs/build/latex/anacondaopentelemetrywrapper.pdf dist/anaconda-opentelemetry-${__SDK_VERSION__}.pdf
-
-# Produce conda package
-conda build --output-folder ./conda-recipe/build conda-recipe
-[[ $? -ne 0 ]] && echo "ERROR: conda build failed!" >&2 && restore_version && exit 1
-restore_version
-
-cp conda-recipe/build/noarch/anaconda-opentelemetry-${__SDK_VERSION__}-py_0.conda dist/
 
 # Clean up generated files
 rm -rf docs/source/anaconda*opentelemetry.rst version.txt
